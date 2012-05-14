ed453e469706475f7a6469f1b79e6d531841bea0
etherglow <etherglow@gmail.com>
2012-05-14 17:01:06 +0700

    Added error handling for FileNotFoundException(returns "File not found"); now we can separate "Denied" and "Not enrolled yet" cases

--- Verificator/src/com/budiw/fpapplet/VerificatorServer.java
+++ Verificator/src/com/budiw/fpapplet/VerificatorServer.java
@@ -1,6 +1,9 @@
 package com.budiw.fpapplet;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import com.digitalpersona.onetouch.DPFPFeatureSet;
 import com.digitalpersona.onetouch.DPFPGlobal;
@@ -14,16 +17,17 @@ public class VerificatorServer {
 	private static DPFPFeatureSet uploadedFeatureSet;
 	private static DPFPTemplate storedTemplate;
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		if(args.length < 2) {
-			System.err.println("ARGS:\n [0]: featureSetName\n [1]: templateName");
+			System.err.println("ARGS:\n [0]: templateName\n [1]: featureName\n");
 		}
 		
+		System.out.println(new File(".").getAbsolutePath());
+		
+		FileInputStream stream = null;
 		try {
 			//Read template file
-			FileInputStream stream = new FileInputStream(args[0]);
-			
-
+			stream = new FileInputStream(args[0]);
 			byte[] data = new byte[stream.available()];
 			stream.read(data);
 			stream.close();
@@ -36,20 +40,23 @@ public class VerificatorServer {
 			stream.read(data);
 			stream.close();
 			uploadedFeatureSet = DPFPGlobal.getFeatureSetFactory().createFeatureSet(data);
+			
+			DPFPVerificationResult result = verificator.verify(uploadedFeatureSet, storedTemplate);
+			if (result.isVerified()) {
+				System.out.println("VERIFIED");
+			} else {
+				System.out.println("DENIED");
+			}
 
+		} catch(FileNotFoundException e) {
+			System.out.println("File not found");
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());;
 			e.printStackTrace();
-		}
-		
-		
-		
-		DPFPVerificationResult result = verificator.verify(uploadedFeatureSet, storedTemplate);
-		if (result.isVerified()) {
-			System.out.println("VERIFIED");
-		} else {
-			System.out.println("DENIED");
+		} finally {
+			if(stream!=null) {
+				stream.close();
+			}
 		}
 	}
-
 }
